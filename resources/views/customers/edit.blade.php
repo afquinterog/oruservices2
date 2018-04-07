99949be7f0d048fe6f376340f949b383fa76d788
oru <orutechnologies@gmail.com>
2018-04-06 22:21:11 -0500

    Changes made for back at main page on click in the button that specify the actual option

--- resources/views/customers/edit.blade.php
+++ resources/views/customers/edit.blade.php
@@ -9,7 +9,7 @@
 	  @slot('breadcrumbs')
 	  	<ol class="breadcrumb">
 	        <li class="breadcrumb-item"><a href="/dashboard">Inicio</a></li>
-	        <li class="breadcrumb-item active"><a href="#"> {{ __('messages.customers_title') }} </a></li>
+	        <li class="breadcrumb-item active"><a href="/customers"> {{ __('messages.customers_title') }} </a></li>
 	      </ol>
 	  @endslot
 
--- resources/views/servicetypes/edit.blade.php
+++ resources/views/servicetypes/edit.blade.php
@@ -9,7 +9,7 @@
 	  @slot('breadcrumbs')
 	  	<ol class="breadcrumb">
 	        <li class="breadcrumb-item"><a href="/dashboard">Inicio</a></li>
-	        <li class="breadcrumb-item active"><a href="#"> {{ __('messages.service_types_title') }} </a></li>
+	        <li class="breadcrumb-item active"><a href="/service-types"> {{ __('messages.service_types_title') }} </a></li>
 	      </ol>
 	  @endslot
 
